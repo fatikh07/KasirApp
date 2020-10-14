38764bff944ed1a94c8f8fff9df1c39569c193cd
fatikh07 <fatihuddin5@gmail.com>
2020-10-14 21:10:42 +0700

    Tutorial 10

---
+++ kasir-app/src/components/ModalKeranjang.js
@@ -0,0 +1,102 @@
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { numberWithComas } from "../utils/utils";
+
+const ModalKeranjang = ({
+  showModal,
+  handleClose,
+  keranjangDetail,
+  jumlah,
+  keterangan,
+  tambah,
+  kurang,
+  changeHandler,
+  handleSubmit,
+}) => {
+  if (keranjangDetail) {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {keranjangDetail.product.nama}{" "}
+            <strong>
+              (Rp. {numberWithComas(keranjangDetail.product.harga)})
+            </strong>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Total Harga :</Form.Label>
+              <strong>
+                <p>Rp. {numberWithComas(keranjangDetail.total_harga)}</p>
+              </strong>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Jumlah :</Form.Label>
+              <br />
+              <Button
+                variant="primary"
+                size="sm"
+                className="mr-2"
+                onClick={() => tambah()}
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+              <strong>{jumlah}</strong>
+              <Button
+                variant="primary"
+                size="sm"
+                className="ml-2"
+                onClick={() => kurang()}
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Keterangan :</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                name="keterangan"
+                placeholder="Contoh : Pedas, Nasi Setengah"
+                value={keterangan}
+                onChange={(event) => changeHandler(event)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Simpan
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger">
+            <FontAwesomeIcon icon={faTrash} />
+            &nbsp; Hapus Pesanan
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  } else {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Kosong</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Kosong</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+};
+
+export default ModalKeranjang;
